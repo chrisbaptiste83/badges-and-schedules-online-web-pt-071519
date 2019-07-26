37fda84ec928389b8ce88e848ff82f03ff442f54
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-26 14:54:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,13 +2,3 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 end 
 
-def batch_badge_creator(names)
-  new_array=[]
-  names.each{|name| puts new_array.push("Hello, my name is #{name}")
-  new_array
-end 
-end 
-
-def assign_rooms(speakers)
-  
-  end
